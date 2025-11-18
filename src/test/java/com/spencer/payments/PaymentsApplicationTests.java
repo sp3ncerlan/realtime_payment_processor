52b9d31db240f6a19d29cfd7adb36e1b725c7ed2
sp3ncerlan <lansp329@gmail.com>
2025-11-18 13:46:15 -0600

    project + docker (postgresql, redis) set up

---
+++ src/test/java/com/spencer/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spencer.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
